7506a0fa3eb3e74a8ed1ab49be3ed4ee1af28d6f
qin2007 <qin@ractoc.com>
2025-08-04 13:18:32 +0200

    0.0.11

--- src/main.ts
+++ src/main.ts
@@ -3,20 +3,12 @@ import { Devvit, Post, TriggerContext, Comment } from '@devvit/public-api';
 
 Devvit.configure({ redditAPI: true, });
 
-Devvit.addSettings([
-    {
-        type: 'select',
-        name: 'action',
-        label: 'what to do with guilty',
-        options: [
-            { label: 'ban user', value: 'banUser' },
-            { label: 'remove comment or post', value: 'remove' },
-            { label: 'report (or modmail if remove is chosen)', value: 'report' },
-        ],
-        defaultValue: ['report'],
-        multiSelect: true,
-    },
-]);
+// Devvit.addSettings([{type: 'select', name: 'action',
+// label: 'what to do with guilty',options:
+// [ { label: 'ban user', value: 'banUser' },
+// { label: 'remove comment or post', value: 'remove' },
+// { label: 'report (or modmail if remove is chosen)', value: 'report' },
+// ],defaultValue: ['report'],multiSelect: true,},]);
 
 Devvit.addTrigger({
     events: ['PostCreate', 'PostUpdate', 'CommentCreate', 'CommentUpdate'],
@@ -38,26 +30,19 @@ Devvit.addTrigger({
     },
 });
 
-async function onThingUpdate(body: string, user: UserV2, context: TriggerContext, item: Post | Comment) {
-    const isGuilty = validateStringText(body), actions: string[] = (await context.settings.get('action')) ?? [];
-    const subredditName = await context.reddit.getCurrentSubredditName();
-    const username = user.name;
+async function onThingUpdate(body: string, _user: UserV2, context: TriggerContext, item: Post | Comment) {
+    const isGuilty = validateStringText(body);//, actions: string[] = (await context.settings.get('action')) ?? [];
+    const subredditName = await context.reddit.getCurrentSubredditName();//, username = user.name;
     if (!subredditName) return;
     if (isGuilty) {
-        if (actions.includes('banUser')) {
-            context.reddit.banUser({
-                context: item.id, subredditName, username,
-                message: 'you have been banned for stating your age\n\nnote: i may be incorrect because im a bot'
-            });
-        }
-        if (actions.includes('remove')) item.remove(); else
-            if (actions.includes('report')) context.reddit.report(item, {
-                reason: `this user might state their age (${context.appVersion})`,
-            });
+        // if (actions.includes('banUser')) {context.reddit.banUser({context: item.id, subredditName, username,
+        // message: 'you have been banned for stating your age\n\nnote: i may be incorrect because im a bot'
+        // });} if (actions.includes('remove')) item.remove(); else if (actions.includes('report'))
+        await context.reddit.report(item, { reason: `this user might state their age (${context.appVersion}) {{${isGuilty}}}`, });
     }
 }
 
-function validateStringText(body: string): boolean {
+function validateStringText(body: string): any {
     const numberWords = {
         one: 1, two: 2, three: 3, four: 4, five: 5,
         six: 6, seven: 7, eight: 8, nine: 9, ten: 10,
@@ -70,22 +55,36 @@ function validateStringText(body: string): boolean {
         .toLowerCase().trim().replace(/[^a-z0-9]/g, '');
     const comparisons = [
         /i(?:am|m)?a?minor/,
-        /i(?:am|m)?a?\d{1,2}/,
-        /i(?:am|m)?under\d{1,2}/,
         /turn(?:ed|ing)\d{1,2}/,
+        /i(?:am|m)?under\d{1,2}/,
+        /i(?:am|m)?(?:a|like)?\d{1,2}/,
         /i(?:am|m)?(?:about|around|almost|over|past|inmy|recently|just|nearly)\d{1,2}/,
+        /i(?:am|m)?a?(?:teen|teenager|adult|senior)/,
         /\d{1,2}(?:r?st|nd|rd|th)?b(?:irth)?day/,
-        /i(?:am|m)?celebratingMy\d{1,2}/i, 
+        /i(?:am|m)?celebratingMy\d{1,2}/i,
     ];
-    if (comparisons.some(regex => regex.test(text))) return true;
+    const { value } = findFirstTruthy(comparisons, regex => regex.test(text));
+    // if (comparisons.some(regex => regex.test(text))) return true;
+    if (value) return (value.exec(text) ?? [true])[0];
     for (const regex of comparisons) {
         for (let word in numberWords) {
-            if (RegExp(regex.source.replace('\\d{1,2}', word.replace('y','(?:y|ies?)')), regex.flags).test(text)) {
-                return true;
-            }
+            const exec = RegExp(regex.source.replace('\\d{1,2}', word.replace('y', '(?:y|ies?)')), regex.flags).exec(text);
+            if (exec) return exec[0];
         }
     }
     return false;
+}
+
+function findFirstTruthy<T>(array: T[], callback: (mixed: T, index: number) => boolean | any, thisContext?: any): { callbackReturn: any, value: T | null } {
+    let i = 0;
+    {
+        for (const value of array) {
+            const callbackReturn = Function.prototype.call.call(callback, thisContext, value, i++);
+            if (callbackReturn) return { callbackReturn, value };
+        }
+    }
+    const callbackReturn = null, value = null;
+    return { callbackReturn, value };
 }
 
 export default Devvit;
